3add599f01067f2fc2eb2b0a0c23c073e14dc4a8
Francisco <franciscojosejr92@gmail.com>
2024-11-13 22:51:41 -0300

    index finalizando

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,13 @@ function selectLink() {
 }
 
 menuItem.forEach((item) => item.addEventListener("click", selectLink))
+
+//Expandi o menu
+
+var btnExp = document.querySelector("#btn-exp")
+var menuSide = document.querySelector(".menu-lateral")
+
+// utilizando o evento para quando for clicado
+btnExp.addEventListener("click", function () {
+  menuSide.classList.toggle("expandir")
+})
