16fae18f14fdfe257d347b49924bf07bf71013a3
Francisco <franciscojosejr92@gmail.com>
2024-11-17 17:03:20 -0300

    atualizando

--- scripts/save.js
+++
@@ -1,9 +0,0 @@
-// Seleciona o botão de menu "hambúrguer" no documento HTML
-const hamburguer = document.querySelector(".hamburguer")
-
-// Seleciona o elemento de navegação que contém a lista de links
-const nav = document.querySelector(".nav")
-
-// uma arrow function para quando clicar no botão hamburguer e for verdadeiro ele vai sumir e quando for falso ele vai permanecer lá
-hamburguer.addEventListener("click", () => nav.classList.toggle("active"))
-// Alterna a classe "active" no elemento de navegação ao clicar no botão
